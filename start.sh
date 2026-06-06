644cb871408b8b7c12c35929ca1aadbabce35fe0
kuromi04 <kuromi04@users.noreply.github.com>
2026-06-06 16:42:23 -0500

    chore(termux): add start.sh script to boot server easily

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/bash
+export PATH="/data/data/com.termux/files/home/Projects/monad-mining-registry/bin:$PATH"
+export NODE_OPTIONS="--require /data/data/com.termux/files/home/Projects/monad-mining-registry/patch-cpus.js"
+yarn start
